396fe0bfaf78d31e24451112264d373977372ab8
Emi <3m1@colossus.local>
2017-05-30 16:19:25 +0200

    Correzioni definitive installazioni shell

--- lnx/install-shell.sh
+++ lnx/install-shell.sh
@@ -35,12 +35,9 @@ then
     sudo apt-get install zsh
 fi
 
-if [[ ! -d  ${HOME}/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git/ ]];
-then
-    echo "Installing Oh-My-Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    chsh -s `which zsh`
-fi
+echo "Installing Oh-My-Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s `which zsh`
 
 if [ $(dpkg-query -W -f='${Status}' neovim 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
@@ -67,6 +64,8 @@ fi
 echo "Installing generics dot configuration files"
 ln -sf ${DOTFILESDIR}/.oh-my-zsh_custom_themes/emiliano.zsh-theme ${HOME}/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git/themes/emiliano.zsh-theme
 ln -sf ${DOTFILESDIR}/nvim/init.vim ${HOME}/.config/nvim/init.vim
+ln -sf ${DOTFILESDIR}/nvim/init.vim ${HOME}/.vimrc
+ln -sf ${DOTFILESDIR}/nvim/nvim_plugins.vim  ${HOME}/nvim_plugins.vim
 ln -sf ${DOTFILESDIR}/.zshrc ${HOME}/.zshrc
 ln -sf ${DOTFILESDIR}/.dockerignore ${HOME}/.dockerignore
 ln -sf ${DOTFILESDIR}/.gitconfig ${HOME}/.gitconfig
@@ -74,15 +73,12 @@ ln -sf ${DOTFILESDIR}/.gitignore_global ${HOME}/.gitignore_global
 ln -sf ${DOTFILESDIR}/.ghci ${HOME}/.ghci
 ln -sf ${DOTFILESDIR}/ghci.conf ${HOME}/ghci.conf
 ln -sf ${DOTFILESDIR}/.git_template ${HOME}/.git_template
-ln -sf ${DOTFILESDIR}/.netrc.gpg ${HOME}/.netrc.gpg
 ln -sf ${DOTFILESDIR}/.scalafmt ${HOME}/.scalafmt
 ln -sf ${DOTFILESDIR}/.vimperatorrc ${HOME}/.vimperatorrc
-ln -sf ${DOTFILESDIR}/nvim/init.vim ${HOME}/.vimrc
-ln -sf ${DOTFILESDIR}/nvim/nvim_plugins.vim  ${HOME}/nvim_plugins.vim
 ln -sf ${DOTFILESDIR}/.vimsessions ${HOME}/.vimsessions
 ln -sf ${DOTFILESDIR}/manuali ${HOME}/manuali
 ln -sf ${DOTFILESDIR}/appunti ${HOME}/appunti
-cp -f ${DOTFILESDIR}/.ssh/config ${HOME}/.ssh
+ln -sf ${DOTFILESDIR}/.ssh/config ${HOME}/.ssh/config
 
 echo "Installing specifics dot configuration files"
 ln -sf ${DOTFILESDIR}/lnx/.zshenv ${HOME}/.zshenv
--- mac/install-shell.sh
+++ mac/install-shell.sh
@@ -2,8 +2,14 @@
 
 DOTFILESDIR=${HOME}/dotfiles
 
-echo "Installing Homebrew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew help > /dev/null;
+
+if [ $? -eq 0 ]; then
+    echo "Homebrew already installed"
+else
+    echo "Installing Homebrew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 if brew ls --versions git > /dev/null; then
     echo "Git already installed"
@@ -19,12 +25,16 @@ else
     brew install curl
 fi
 
-if [[ ! -d ${HOME}/.antigen ]]; then
+if [[ -d ${HOME}/.antigen ]]; then
+    echo "Antigen already installed"
+else
     echo "Installing Antigen..."
     git clone https://github.com/zsh-users/antigen.git ${HOME}/.antigen
 fi
 
-if [[ ! -d ${HOME}/dein.vim ]]; then
+if [[ -d ${HOME}/dein.vim ]]; then
+    echo "Dein.vim already installed"
+else
     echo "Installing dein.vim..."
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
     sh ./installer.sh ${HOME}
@@ -33,15 +43,13 @@ fi
 if brew ls --versions zsh > /dev/null; then
     echo "Zsh already installed"
 else
-    echo "Installing zsh..."
+    echo "Installing Zsh..."
     brew install zsh zsh-completions
     chsh -s $(which zsh)
 fi
 
-if [[ ! -d ${HOME}/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git ]]; then
-    echo "Installing Oh-My-Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-if
+echo "Installing Oh-My-Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 if brew ls --versions neovim > /dev/null; then
     echo "Neovim already installed"
@@ -68,7 +76,9 @@ fi
 echo "Installing generics dot configuration files"
 ln -sf ${DOTFILESDIR}/.oh-my-zsh_custom_themes/emiliano.zsh-theme ${HOME}/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git/themes/emiliano.zsh-theme
 ln -sf ${DOTFILESDIR}/nvim/init.vim ${HOME}/.config/nvim/init.vim
-ln -sf ${DOTFILESDIR}/.zshenv ${HOME}/.zshenv
+ln -sf ${DOTFILESDIR}/nvim/init.vim ${HOME}/.vimrc
+ln -sf ${DOTFILESDIR}/nvim/nvim_plugins.vim  ${HOME}/nvim_plugins.vim
+ln -sf ${DOTFILESDIR}/.zshrc ${HOME}/.zshrc
 ln -sf ${DOTFILESDIR}/.docker ${HOME}/.docker
 ln -sf ${DOTFILESDIR}/.dockerignore ${HOME}/.dockerignore
 ln -sf ${DOTFILESDIR}/.gitconfig ${HOME}/.gitconfig
@@ -76,20 +86,16 @@ ln -sf ${DOTFILESDIR}/.gitignore_global ${HOME}/.gitignore_global
 ln -sf ${DOTFILESDIR}/.ghci ${HOME}/.ghci
 ln -sf ${DOTFILESDIR}/ghci.conf ${HOME}/ghci.conf
 ln -sf ${DOTFILESDIR}/.git_template ${HOME}/.git_template
-ln -sf ${DOTFILESDIR}/.netrc.gpg ${HOME}/.netrc.gpg
 ln -sf ${DOTFILESDIR}/.scalafmt ${HOME}/.scalafmt
 ln -sf ${DOTFILESDIR}/.vimperatorrc ${HOME}/.vimperatorrc
-ln -sf ${DOTFILESDIR}/nvim/init.vim ${HOME}/.vimrc
-ln -sf ${DOTFILESDIR}/nvim/nvim_plugins.vim  ${HOME}/nvim_plugins.vim
 ln -sf ${DOTFILESDIR}/.vimsessions ${HOME}/.vimsessions
 ln -sf ${DOTFILESDIR}/manuali ${HOME}/manuali
 ln -sf ${DOTFILESDIR}/appunti ${HOME}/appunti
-cp -f  ${DOTFILESDIR}/.ssh/config ${HOME}/.ssh/config
+ln -sf  ${DOTFILESDIR}/.ssh/config ${HOME}/.ssh/config
 
 echo "Installing specifics dot configuration files"
-ln -sf ${DOTFILESDIR}/mac/.zshrc ${HOME}/.zshrc
+ln -sf ${DOTFILESDIR}/mac/.zshenv ${HOME}/.zshenv
 ln -sf ${DOTFILESDIR}/mac/.osx ${HOME}/.osx
 ln -sf ${DOTFILESDIR}/mac/.profile ${HOME}/.profile
 ln -sf ${DOTFILESDIR}/mac/.tmux.conf ${HOME}/.tmux.conf
 ln -sf ${DOTFILESDIR}/mac/.vimperatorsys ${HOME}/.vimperatorsys
-ln -sf ${DOTFILESDIR}/mac/Library/Application Support/Karabiner/private.xml ${HOME}/Library/Application Support/Karabiner/private.xml
