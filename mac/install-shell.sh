8c556e0921d4cb51111f6290a0d87ac5ebc10747
Emi <3m1@colossus.local>
2017-05-30 15:35:29 +0200

    Modificato script installazione mac shell

--- mac/install-shell.sh
+++ mac/install-shell.sh
@@ -1,63 +1,95 @@
 #!/usr/bin/env bash
 
-BASE_DIR=$(dirname $0)
-PARENT_DIR=$(dirname $BASE_DIR)
+DOTFILESDIR=${HOME}/dotfiles
 
 echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Installing git..."
-brew install git
+if brew ls --versions git > /dev/null; then
+    echo "Git already installed"
+else
+    echo "Installing git..."
+    brew install git
+fi
 
-echo "Installing curl..."
-brew install curl
+if brew ls --versions curl > /dev/null; then
+    echo "curl already installed"
+else
+    echo "Installing curl..."
+    brew install curl
+fi
 
-echo "Installing Antigen..."
-rm -fr ${HOME}/.antigen
-git clone https://github.com/zsh-users/antigen.git ${HOME}/.antigen
+if [[ ! -d ${HOME}/.antigen ]]; then
+    echo "Installing Antigen..."
+    git clone https://github.com/zsh-users/antigen.git ${HOME}/.antigen
+fi
 
-echo "Installing dein.vim..."
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ${HOME}
+if [[ ! -d ${HOME}/dein.vim ]]; then
+    echo "Installing dein.vim..."
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+    sh ./installer.sh ${HOME}
+fi
 
-echo "Installing zsh..."
-brew install zsh zsh-completions
-chsh -s $(which zsh)
+if brew ls --versions zsh > /dev/null; then
+    echo "Zsh already installed"
+else
+    echo "Installing zsh..."
+    brew install zsh zsh-completions
+    chsh -s $(which zsh)
+fi
 
-echo "Installing Oh-My-Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if [[ ! -d ${HOME}/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git ]]; then
+    echo "Installing Oh-My-Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+if
 
-echo "Installing Neovim..."
-brew install neovim/neovim/neovim
+if brew ls --versions neovim > /dev/null; then
+    echo "Neovim already installed"
+else
+    echo "Installing Neovim..."
+    brew install neovim/neovim/neovim
+fi
 
-echo "Installing Ripgrep..."
-brew install ripgrep
+if brew ls --versions zsh > /dev/null; then
+    echo "Zsh already installed"
+else
+    echo "Installing zsh..."
+    brew install zsh zsh-completions
+    chsh -s $(which zsh)
+fi
+
+if brew ls --versions ripgrep > /dev/null; then
+    echo "Ripgrep already installed"
+else
+    echo "Installing Ripgrep..."
+    brew install ripgrep
+fi
 
 echo "Installing generics dot configuration files"
-ln -sf ${PARENT_DIR}/.oh-my-zsh_custom_themes/emiliano.zsh-theme ${HOME}/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git/themes/emiliano.zsh-theme
-ln -sf ${PARENT_DIR}/nvim/init.vim ${HOME}/.config/nvim/init.vim
-ln -sf ${PARENT_DIR}/.zshenv ${HOME}/.zshenv
-ln -sf ${PARENT_DIR}/.docker ${HOME}/.docker
-ln -sf ${PARENT_DIR}/.dockerignore ${HOME}/.dockerignore
-ln -sf ${PARENT_DIR}/.gitconfig ${HOME}/.gitconfig
-ln -sf ${PARENT_DIR}/.gitignore_global ${HOME}/.gitignore_global
-ln -sf ${PARENT_DIR}/.ghci ${HOME}/.ghci
-ln -sf ${PARENT_DIR}/ghci.conf ${HOME}/ghci.conf
-ln -sf ${PARENT_DIR}/.git_template ${HOME}/.git_template
-ln -sf ${PARENT_DIR}/.netrc.gpg ${HOME}/.netrc.gpg
-ln -sf ${PARENT_DIR}/.scalafmt ${HOME}/.scalafmt
-ln -sf ${PARENT_DIR}/.vimperatorrc ${HOME}/.vimperatorrc
-ln -sf ${PARENT_DIR}/nvim/init.vim ${HOME}/.vimrc
-ln -sf ${PARENT_DIR}/nvim/nvim_plugins.vim  ${HOME}/nvim_plugins.vim
-ln -sf ${PARENT_DIR}/.vimsessions ${HOME}/.vimsessions
-ln -sf ${PARENT_DIR}/manuali ${HOME}/manuali
-ln -sf ${PARENT_DIR}/appunti ${HOME}/appunti
-cp -f ${PARENT_DIR}/.ssh/config ${HOME}/.ssh/config
+ln -sf ${DOTFILESDIR}/.oh-my-zsh_custom_themes/emiliano.zsh-theme ${HOME}/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git/themes/emiliano.zsh-theme
+ln -sf ${DOTFILESDIR}/nvim/init.vim ${HOME}/.config/nvim/init.vim
+ln -sf ${DOTFILESDIR}/.zshenv ${HOME}/.zshenv
+ln -sf ${DOTFILESDIR}/.docker ${HOME}/.docker
+ln -sf ${DOTFILESDIR}/.dockerignore ${HOME}/.dockerignore
+ln -sf ${DOTFILESDIR}/.gitconfig ${HOME}/.gitconfig
+ln -sf ${DOTFILESDIR}/.gitignore_global ${HOME}/.gitignore_global
+ln -sf ${DOTFILESDIR}/.ghci ${HOME}/.ghci
+ln -sf ${DOTFILESDIR}/ghci.conf ${HOME}/ghci.conf
+ln -sf ${DOTFILESDIR}/.git_template ${HOME}/.git_template
+ln -sf ${DOTFILESDIR}/.netrc.gpg ${HOME}/.netrc.gpg
+ln -sf ${DOTFILESDIR}/.scalafmt ${HOME}/.scalafmt
+ln -sf ${DOTFILESDIR}/.vimperatorrc ${HOME}/.vimperatorrc
+ln -sf ${DOTFILESDIR}/nvim/init.vim ${HOME}/.vimrc
+ln -sf ${DOTFILESDIR}/nvim/nvim_plugins.vim  ${HOME}/nvim_plugins.vim
+ln -sf ${DOTFILESDIR}/.vimsessions ${HOME}/.vimsessions
+ln -sf ${DOTFILESDIR}/manuali ${HOME}/manuali
+ln -sf ${DOTFILESDIR}/appunti ${HOME}/appunti
+cp -f  ${DOTFILESDIR}/.ssh/config ${HOME}/.ssh/config
 
 echo "Installing specifics dot configuration files"
-ln -sf ${BASE_DIR}/.zshrc ${HOME}/.zshrc
-ln -sf ${BASE_DIR}/.osx ${HOME}/.osx
-ln -sf ${BASE_DIR}/.profile ${HOME}/.profile
-ln -sf ${BASE_DIR}/.tmux.conf ${HOME}/.tmux.conf
-ln -sf ${BASE_DIR}/.vimperatorsys ${HOME}/.vimperatorsys
-ln -sf ${BASE_DIR}/Library/Application Support/Karabiner/private.xml ${HOME}/Library/Application Support/Karabiner/private.xml
+ln -sf ${DOTFILESDIR}/mac/.zshrc ${HOME}/.zshrc
+ln -sf ${DOTFILESDIR}/mac/.osx ${HOME}/.osx
+ln -sf ${DOTFILESDIR}/mac/.profile ${HOME}/.profile
+ln -sf ${DOTFILESDIR}/mac/.tmux.conf ${HOME}/.tmux.conf
+ln -sf ${DOTFILESDIR}/mac/.vimperatorsys ${HOME}/.vimperatorsys
+ln -sf ${DOTFILESDIR}/mac/Library/Application Support/Karabiner/private.xml ${HOME}/Library/Application Support/Karabiner/private.xml
