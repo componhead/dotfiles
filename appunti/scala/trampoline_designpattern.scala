4db5eccc79ca2645e4af2bf30a2740278b4ab522
Emi <hate@spam.bot>
2017-06-13 14:09:38 +0200

    Spostamento appunti in repository mynotes

--- appunti/scala/trampoline_designpattern.scala
+++
@@ -1,9 +0,0 @@
-sealed Trait TailRec[A]
-case class Return[A](a: A) extends TailRec
-case class Suspend[A](resume: () => TailRec[A]) extends TailRec[A]
-
-  def composeAll[A](fs: List[A => A]): A => A = {
-    a1 => (fs.foldRight(Return(_)) {
-      (f,r) => a2 => Suspend(() => f(r(a2)))
-    })(a1.run)
-  }
--- appunti/scala/whitebook.md
+++
@@ -1,5 +0,0 @@
-**TRAITS**
-A trait cannot have any "class" parameters (i.e., parameters passed to the primary constructor of a class). Whereas in classes, super calls are statically bound, in traits, they are dynamically bound. 
-
-Generally, a for expression is of the form:
-`for ( seq ) yield expr`
