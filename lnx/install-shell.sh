7a6ca0de1238f71450190d80bd88c1afc085570e
Emi <i.hate@spam.bot>
2017-06-19 17:24:53 +0200

    Installazione di oh-my-fish

--- lnx/install-shell.sh
+++ lnx/install-shell.sh
@@ -39,6 +39,7 @@ sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
 sudo apt-get install fish
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+chsh -s /usr/local/bin/fish
 
 echo "Installing oh-my-fish shell..."
 curl -L https://get.oh-my.fish > ~/install
