9a8669e9dac7ad748f02e71cfb00b6327419598d
Emi <i.hate@spam.bot>
2017-06-20 10:49:15 +0200

    Aggiunte variabile a fish

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,4 +1,3 @@
-set -xg PATH $PATH:~/.local/bin:~/.nexustools
 set -xg TERM screen-256color
 set -xg CLICOLOR 1
 set -xg CLICOLOR_FORCE 1
@@ -8,3 +7,6 @@ set -xg GREP_COLOR '3;33'
 set -xg JEKYLL_GITHUB_TOKEN 'c5826f89fba802f901cb846fbaf5e3f3059e51af'
 set -xg EDITOR nvim
 set -xg DOCKERFILES "$DOTFILES/dockerFiles/"
+set -xg LANG en_US.UTF-8
+set -xg fish_user_paths /usr/local/bin $fish_user_paths
+
