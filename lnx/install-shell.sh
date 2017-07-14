02ca013a2e033eab04a910a98c04dedf4245fd42
Emi <hate@spam.bot>
2017-07-14 11:52:34 +0200

    Correzioni installer lnx (ubunutu/kali)

--- lnx/install-shell.sh
+++ lnx/install-shell.sh
@@ -3,7 +3,23 @@
 # Installation on UBUNTU (14.04+)
 DOTFILESDIR=${HOME}/dotfiles
 
-mkdir ~/bin
+DISTRO=sudo cat /etc/lsb-release | sudo grep "DISTRIB_ID" | sudo sed "s/\w\+=\(\w\+\)$/\1/"
+
+if [ ! -d ~/bin ]
+then
+    mkdir ~/bin
+fi
+
+if [ ! -d ~/.local ]
+then
+    mkdir -p ~/.local/bin
+    mkdir -p ~/.local/share
+fi
+
+if [ ! -d ~/.config ]
+then
+    mkdir ~/.config
+fi
 
 if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "git already installed") -eq 0 ];
 then
@@ -20,9 +36,9 @@ fi
 echo "******* Installing Fura Nerd font..."
 if [ ! -d /usr/share/fonts/opentype ]
 then
-    mkdir -p /usr/share/fonts/opentype
+    mkdir -p /usr/share/fonts/opentype/FuraCodeRegularNerd
 fi
-curl -fLo /usr/share/fonts/opentype/Fura\ Code\ Regular\ Nerd\ Complete.otf https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fura%20Code%20Regular%20Nerd%20Font%20Complete.otf
+curl -fLo /usr/share/fonts/opentype/FuraCodeRegularNerd/Fura\ Code\ Regular\ Nerd\ Complete.otf https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fura%20Code%20Regular%20Nerd%20Font%20Complete.otf
 sudo fc-cache -fv
 
 echo "******* Installing neovim Plug..."
@@ -30,7 +46,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
 
 
 echo "******* Installing gpg tools..."
-sudo apt-get install -y gpgv2 gnupg2 gnupg-agent gnupg-doc pinentry-curses
+sudo apt-get install -y gpgv2 gnupg2 gnupg-agent pinentry-curses
 curl -o ~/.local/bin/git-credential-netrc https://raw.githubusercontent.com/git/git/master/contrib/credential/netrc/git-credential-netrc
 chmod +x ~/.local/bin/git-credential-netrc
 
@@ -75,21 +91,18 @@ then
     sudo apt-get install -y colordiff
 fi
 
-echo "Installing Ripgrep..."
-wget -O ~/bin/ripgrep.tar.gz https://github.com/BurntSushi/ripgrep/releases/download/0.5.2/ripgrep-0.5.2-x86_64-unknown-linux-musl.tar.gz
-tar xzvf ~/bin/ripgrep.tar.gz ~/bin
-mv ~/bin/ripgrep/rg ~/bin
-rm -r ripgrep*
-
-if [ $(dpkg-query -W -f='${Status}' docker 2>/dev/null | grep -c "docker already installed") -eq 0 ];
+if $DISTRO -eq "Ubuntu";
 then
-    echo "******* Installing Docker..."
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update
-    sudo apt-get install -y docker-ce
-    sudo systemctl status docker
-    sudo usermod -aG docker $(whoami)
+    if [ $(dpkg-query -W -f='${Status}' docker 2>/dev/null | grep -c "docker already installed") -eq 0 ];
+    then
+        echo "******* Installing Docker..."
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        sudo apt-get update
+        sudo apt-get install -y docker-ce
+        sudo systemctl status docker
+        sudo usermod -aG docker $(whoami)
+    fi
 fi
 
 if [ $(dpkg-query -W -f='${Status}' xsel 2>/dev/null | grep -c "xsel already installed") -eq 0 ];
@@ -109,25 +122,6 @@ wget -O ~/bin/fpp.tar.gz https://github.com/facebook/PathPicker/releases/downloa
 tar xzvf ~/bin/fpp.tar.gz ~/bin
 rm ~/bin/fpp.tar.gz
 
-echo "******* Installing generics dot configuration files..."
-ln -sf ${DOTFILESDIR}/.config/nvim/init.vim ${HOME}/.config/nvim/init.vim
-ln -sf ${DOTFILESDIR}/.config/nvim/init.vim ${HOME}/.vimrc
-ln -sf ${DOTFILESDIR}/.config/nvim/nvim_plugins.vim  ${HOME}/nvim_plugins.vim
-ln -sf ${DOTFILESDIR}/.dockerignore ${HOME}/.dockerignore
-ln -sf ${DOTFILESDIR}/.gitconfig ${HOME}/.gitconfig
-ln -sf ${DOTFILESDIR}/.gitignore_global ${HOME}/.gitignore_global
-ln -sf ${DOTFILESDIR}/.ghci ${HOME}/.ghci
-ln -sf ${DOTFILESDIR}/ghci.conf ${HOME}/ghci.conf
-ln -sf ${DOTFILESDIR}/.git_template ${HOME}/
-ln -sf ${DOTFILESDIR}/.scalafmt ${HOME}/.scalafmt
-ln -sf ${DOTFILESDIR}/.vimperatorrc ${HOME}/.vimperatorrc
-ln -sf ${DOTFILESDIR}/.vimsessions ${HOME}/.vimsessions
-ln -sf ${DOTFILESDIR}/.ssh/config ${HOME}/.ssh/config
-ln -sf ${DOTFILESDIR}/.tmux.conf ${HOME}/.tmux.conf
-
-echo "******* Installing specifics dot configuration files"
-ln -sf ${DOTFILESDIR}/lnx/.vimperatorsys ${HOME}/.vimperatorsys
-
 echo "******* Setup Fish shell environment..."
 if [ $(dpkg-query -W -f='${Status}' fish 2>/dev/null | grep -c "fish already installed") -eq 0 ];
 then
@@ -146,7 +140,50 @@ then
     omf install fzf bang-bang bobthefish
 
     rm -r $HOME/.config/omf
-    ln -sf ${DOTFILESDIR}/.config/omf $HOME/.config
     rm -r $HOME/.config/fish
+fi
+
+echo "Installing Ripgrep..."
+wget -O ~/bin/ripgrep.tar.gz https://github.com/BurntSushi/ripgrep/releases/download/0.5.2/ripgrep-0.5.2-x86_64-unknown-linux-musl.tar.gz
+tar xzvf ~/bin/ripgrep.tar.gz -C ~/bin
+mv ~/bin/ripgrep-0.5.2-x86_64-unknown-linux-musl/rg ~/bin
+mv ~/bin/ripgrep-0.5.2-x86_64-unknown-linux-musl/complete/rg.fish ~/.config/fish/functions/
+rm -rf ripgrep-0.5.2-x86_64-unknown-linux-musl
+
+sudo apt autoremove -y
+
+if $DISTRO -eq "Ubuntu";
+then
+    echo "******* Linking generics dot configuration files..."
+    ln -sf ${DOTFILESDIR}/.config/nvim/init.vim ${HOME}/.config/nvim/init.vim
+    ln -sf ${DOTFILESDIR}/.config/nvim/init.vim ${HOME}/.vimrc
+    ln -sf ${DOTFILESDIR}/.config/nvim/nvim_plugins.vim  ${HOME}/nvim_plugins.vim
+    ln -sf ${DOTFILESDIR}/.dockerignore ${HOME}/.dockerignore
+    ln -sf ${DOTFILESDIR}/.gitconfig ${HOME}/.gitconfig
+    ln -sf ${DOTFILESDIR}/.gitignore_global ${HOME}/.gitignore_global
+    ln -sf ${DOTFILESDIR}/.ghci ${HOME}/.ghci
+    ln -sf ${DOTFILESDIR}/ghci.conf ${HOME}/ghci.conf
+    ln -sf ${DOTFILESDIR}/.git_template ${HOME}/
+    ln -sf ${DOTFILESDIR}/.scalafmt ${HOME}/.scalafmt
+    ln -sf ${DOTFILESDIR}/.vimperatorrc ${HOME}/.vimperatorrc
+    ln -sf ${DOTFILESDIR}/.vimsessions ${HOME}/.vimsessions
+    ln -sf ${DOTFILESDIR}/.ssh/config ${HOME}/.ssh/config
+    ln -sf ${DOTFILESDIR}/.tmux.conf ${HOME}/.tmux.conf
+    ln -sf ${DOTFILESDIR}/lnx/.vimperatorsys ${HOME}/.vimperatorsys
+    ln -sf ${DOTFILESDIR}/.config/omf $HOME/.config
     ln -sf ${DOTFILESDIR}/.config/fish $HOME/.config
+else if $DISTRO -eq "Kali";
+    echo "******* Cpying generics dot configuration files..."
+    cp -fr ${DOTFILESDIR}/.config/nvim ${HOME}/.config/nvim
+    cp -f ${DOTFILESDIR}/.config/nvim/init.vim ${HOME}/.vimrc
+    cp -f ${DOTFILESDIR}/.config/nvim/nvim_plugins.vim  ${HOME}/nvim_plugins.vim
+    cp -f ${DOTFILESDIR}/.gitconfig ${HOME}/.gitconfig
+    cp -f ${DOTFILESDIR}/.gitignore_global ${HOME}/.gitignore_global
+    cp -f ${DOTFILESDIR}/.git_template ${HOME}/
+    cp -f ${DOTFILESDIR}/.scalafmt ${HOME}/.scalafmt
+    cp -f ${DOTFILESDIR}/.ssh/config ${HOME}/.ssh/config
+    cp -f ${DOTFILESDIR}/.tmux.conf ${HOME}/.tmux.conf
+    cp -rf ${DOTFILESDIR}/.config/omf $HOME/.config
+    cp -rf ${DOTFILESDIR}/.config/fish $HOME/.config
+    rm -rf ${DOTFILESDIR}
 fi
