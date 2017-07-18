fb51fe0b5259a1f3ecf6f21991314dd5cd37214b
Emi <i.hate@spam.bot>
2017-07-18 16:56:28 +0200

    Affinato abbr add-ssh-keys

--- .config/fish/functions/add-ssh-keys.fish
+++ .config/fish/functions/add-ssh-keys.fish
@@ -1,3 +1,6 @@
 function add-ssh-keys
-    ssh-add ~/.ssh/id_rsa ~/.ssh/work_onetech
+    set keys (ssh-add -l | grep -e 'id_rsa' -e 'work_onetech' | wc -l)
+    if [ $keys -ne 2 ]
+        ssh-add ~/.ssh/id_rsa ~/.ssh/work_onetech
+    end
 end
