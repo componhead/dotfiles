389f77b4676a0c5ababe4deae82105720c01a6dd
Emi <hate@spam.bot>
2017-07-26 13:20:44 +0200

    Tolto sha256sum e messo shasum

--- .config/fish/functions/genpsw.fish
+++ .config/fish/functions/genpsw.fish
@@ -1,5 +1,5 @@
 function genpsw
-    set psw (date +%s | sha256sum | base64 | head -c $argv)
+    set psw (date +%s | shasum | base64 | head -c $argv)
     echo $psw
     echo $psw | eval $CPCLIPBOARD -selection clipboard
 end
