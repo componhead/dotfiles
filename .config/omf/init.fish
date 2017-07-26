bc53a1372e4d009845be6619d234694de1088a46
Emi <i.hate@spam.bot>
2017-07-26 08:33:46 +0200

    Aggiunto editor per fpp

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -9,6 +9,7 @@ set -xg LSCOLOR Gxfxcxdxbxegedabagacad
 set -xg GREP_COLOR '3;33'
 set -xg JEKYLL_GITHUB_TOKEN 'c5826f89fba802f901cb846fbaf5e3f3059e51af'
 set -xg EDITOR nvim
+set -xg FPP_EDITOR nvim
 set -xg DOCKERFILES "$DOTFILES/dockerFiles/"
 set -xg LANG en_US.UTF-8
 set -xg fish_user_paths /usr/local/bin $fish_user_paths
