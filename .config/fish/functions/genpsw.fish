612416cc2dc4b74ee221a20a16316129d23a363c
Emi <i.hate@spam.bot>
2017-08-09 09:38:42 +0200

    Tolto l'escape char nella generazione password

--- .config/fish/functions/genpsw.fish
+++ .config/fish/functions/genpsw.fish
@@ -2,7 +2,7 @@ function genpsw
     set -g digitseeds 1 2 3 4 5 6 7 8 9 0
     set -g alfaseeds q w e r t y u i o p a s d f g h j k l z x c v b n m Q W E R T Y U I O P A S D F G H J K L Z X C V B N M
     set -g hexseeds 1 2 3 4 5 6 7 8 9 0 a b c d e f
-    set -g specialseeds \! \@ \# \$ \% \^ \& \* \( \) \_ \+ \- \= \: \" \| \; \' \\ \< \> \? \, \. \/ \~ \` \{ \} \[ \]
+    set -g specialseeds \! \@ \# \$ \% \^ \& \* \( \) \_ \+ \- \= \: \" \| \; \' \< \> \? \, \. \/ \~ \` \{ \} \[ \]
     set -g seeds $digitseeds $alfaseeds $specialseeds
     if set -q argv[2]
         switch $argv[2]
