cd8d1a4d65f860474f5c7e025d54ee30c67486ee
Emi <i.hate@spam.bot>
2017-09-08 17:33:13 +0200

    Modificata funzione aggiunta chiavi ssh

--- .config/fish/functions/add-ssh-keys.fish
+++ .config/fish/functions/add-ssh-keys.fish
@@ -1,6 +1,10 @@
 function add-ssh-keys
-    set keys (ssh-add -l | grep -e 'id_rsa' -e 'work_onetech' | wc -l)
-    if [ $keys -ne 2 ]
-        ssh-add ~/.ssh/id_rsa ~/.ssh/work_onetech
+    set myid (ssh-add -l | grep -e 'id_rsa' | wc -l)
+    if [ $myid -ne 1 ]
+        ssh-add ~/.ssh/id_rsa
+    end
+    set ot (ssh-add -l | grep -e 'work_onetech' | wc -l)
+    if [ $ot -ne 1 ]
+        ssh-add ~/.ssh/work_onetech
     end
 end
