66e0f698d5caa4795de8b752ba9b0b3ecc4b6301
Emi <i.hate@spam.bot>
2017-10-03 15:44:54 +0200

    Protezione clipboard in rst

--- .config/fish/functions/rst.fish
+++ .config/fish/functions/rst.fish
@@ -2,4 +2,5 @@ function rst
     echo -ne '\ec\e[3J'
     commandline -f repaint
     fish_greeting
+    echo "" | eval $CPCLIPBOARD -selection clipboard
 end
--- mac/specific-configuration.fish
+++ mac/specific-configuration.fish
@@ -1,2 +1,3 @@
+set -xg CPCLIPBOARD pbcopy
 set -xg TMUX_CLIPBOARD_COMMAND "reattach-to-user-namespace pbcopy"
 set -xg LSCOLORS gxcxahdxacafxfaexeabxb
