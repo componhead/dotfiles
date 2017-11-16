f19b7cf01621bc8a68a5dc90a52024a8dc5c3757
Emi <ihate@spam.bot>
2017-11-16 10:47:28 +0100

    Affinato comportamento multigit

--- .config/fish/functions/multigit.fish
+++ .config/fish/functions/multigit.fish
@@ -1,19 +1,22 @@
 function multigit
     set -l basedir $PWD
     if set -q argv[1]
-        switch $argv[1]
-            case push pull fetch
-		for gitdir in (find . -maxdepth 2 -type d -name .git)
-			set -l i (echo $gitdir | sed s/\\.git// | sed s/^\\.\\///)
-			set -l dir (string join "/" $basedir $i)
-			cd $dir ; git $argv[1]
-			echo "  * I'm operating" $argv[1] "on" $dir
-		end
-		cd $basedir
-	    case '*'
-		echo "Comando git sconosciuto o non supportato"
-	end
-   else
-	multigit pull
-   end
+        for gitdir in (find . -type d -name .git)
+	    set -l i (echo $gitdir | sed s/\\.git// | sed s/^\\.\\///)
+	    set -l dir (string join "/" $basedir $i)
+            switch $argv[1]
+                case pull
+	            echo "  * I'm operating" $argv[1] "on" $dir
+	            cd $dir ; git $argv[1] --all --quiet
+                case push fetch
+	            echo "  * I'm operating" $argv[1] "on" $dir
+	            cd $dir ; git $argv[1]
+	        case '*'
+	            echo "Comando git sconosciuto o non supportato"
+            end
+        end
+        cd $basedir
+    else
+        multigit pull
+    end
 end
