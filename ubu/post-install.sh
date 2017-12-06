62ba68fe523987a9dfd23d157c1991015598a6cd
Emi <ihate@spam.bot>
2017-12-06 14:51:14 +0100

    Corretto comportamento bobthefish theme in WSL

--- ubu/post-install.sh
+++ ubu/post-install.sh
@@ -1,8 +1,13 @@
 #! /usr/bin/fish
 echo "******* Installing oh-my-fish shell..."
-curl -L https://get.oh-my.fish > ~/install
+lurl -L https://get.oh-my.fish > ~/install
 /usr/bin/fish ~/install --path=~/.local/share/omf --config=~/.config/omf
 rm ~/install
 
 echo "******* Installing omf plugins..."
+curl -L https://get.oh-my.fish > ~/install
 omf install fzf bang-bang bobthefish fish_logo
+
+# If in WSL:
+# > mv /home/USER/.local/share/omf/pkg/bobthefish /home/USER/.local/share/omf/themes/bobthefish
+# > omf theme bobthefish
