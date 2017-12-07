023329183db967076feb991ad8287c08555ca47d
Emi <ihate@spam.bot>
2017-12-07 09:00:05 +0100

    Cambiamento alias l

--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,16 +1,36 @@
 function l
-    switch (uname)
-        case "Linux"
-            if set -q argv[1]
-                ls -lah --color=always $argv[1] | less -R
-            else
-                ls -lah --color=always | less -R
+    if set -q argv[1]
+        switch $argv[1]
+            case "-c"
+            switch (uname)
+                case "Linux"
+                    if set -q argv[1]
+                        ls -lah --color=always $argv[1] | less -R
+                    else
+                        ls -lah --color=always | less -R
+                    end
+                case "Darwin"
+                    if set -q argv[1]
+                        ls -lah -G $argv[1] | less -R
+                    else
+                        ls -lah -G | less -R
+                    end
             end
-        case "Darwin"
-            if set -q argv[1]
-                ls -lah -G $argv[1] | less -R
-            else
-                ls -lah -G | less -R
-            end
+        end
+    else
+        switch (uname)
+            case "Linux"
+                if set -q argv[1]
+                    ls -lah $argv[1] | less -R
+                else
+                    ls -lah | less -R
+                end
+            case "Darwin"
+                if set -q argv[1]
+                    ls -lah $argv[1] | less -R
+                else
+                    ls -lah | less -R
+                end
+        end
     end
 end
