2a67b6077e59034ad3c1ee1adbb6a041beb72fed
Emi <ihate@spam.bot>
2018-01-03 10:01:56 +0100

    Utilizzo diversificato per risalire alla distribuzione

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,10 +20,14 @@ set -xg DOCKERFILES "$DOTFILES/dockerFiles/"
 set -xg LANG en_US.UTF-8
 set -xg FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 set -xg grepprg rg\ --vimgrep
-set -xg distro (hostnamectl | sed -n "s/^.*Operating System: \(\w\+\) .*\$/\1/p")
+if test -n (hostnamectl | sed -n "s/^.*Operating System: \(\w\+\) .*\$/\1/p")
+    set -xg distro (lsb_release -ds | grep -o "^[a-zA-Z]\+")
+else
+    set -g distro (hostnamectl | sed -n "s/^.*Operating System: \(\w\+\) .*\$/\1/p")
+end
 switch (uname)
     case "Linux"
-        switch $distro
+        switch "$distro"
 		case "Debian" "Ubuntu"
 			set -xg SPECIFIC_DOTFILES "$DOTFILES/ubu"
 		case "Kali"
