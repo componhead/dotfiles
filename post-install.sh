f940e0dd33dce5edb6759968d43c19fb4913d072
Emi <ihate@spam.bot>
2018-01-07 16:24:56 +0100

    Aggiunto plugin omf

--- post-install.sh
+++ post-install.sh
@@ -1,12 +1,16 @@
 #! /usr/bin/fish
 echo "******* Installing oh-my-fish shell..."
 curl -L https://get.oh-my.fish > ~/install
-/usr/bin/fish ~/install --path=~/.local/share/omf --config=~/.config/omf
+fish ~/install --path=~/.local/share/omf --config=~/.config/omf
 rm ~/install
 
 echo "******* Installing omf plugins..."
 curl -L https://get.oh-my.fish > ~/install
-omf install fzf bang-bang bobthefish fish_logo
+omf install \
+    fzf \
+    bobthefish \
+    fish_logo \
+    ssh-term-helper
 
 # If in WSL:
 # > mv /home/USER/.local/share/omf/pkg/bobthefish /home/USER/.local/share/omf/themes/bobthefish
