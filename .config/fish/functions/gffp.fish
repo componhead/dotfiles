90756c7ec4b622defe1517ad31234f2b5deac871
E.A <ihate@spam.bot>
2019-05-17 15:20:16 +0200

    new function

--- .config/fish/functions/gffp.fish
+++ .config/fish/functions/gffp.fish
@@ -1,5 +1,5 @@
 function gffp
     set issue (git branch | grep \* | cut -d ' ' -f2 | sed 's/.*\/\([0-9]*\)$/\1/')
-    echo $(git flow feature publish $issue)
+    printf '%s%d' "git flow feature publish " $issue
 end
 
