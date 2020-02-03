6c6f496a34b3fa60219fe8a88cedf92487fcafdf
emiliano anichini <emiliano.anichini@gmail.com>
2020-02-03 14:39:33 +0100

    Added all functions and all completions

--- mac/specific-configuration.fish
+++ mac/specific-configuration.fish
@@ -4,6 +4,8 @@ set -xg TMUX_CLIPBOARD_COMMAND "reattach-to-user-namespace pbcopy"
 set -xg LSCOLORS gxcxahdxacafxfaexeabxb
 # put on private JAVA_HOME the resulting
 # /usr/libexec/java_home -v 1.8
+set -g fish_user_paths "/usr/local/opt/icu4c/bin" ~/.config/fish/config.fish
+set -g fish_user_paths "/usr/local/opt/icu4c/sbin" ~/.config/fish/config.fish
 
 abbr --add qb ~/opt/qutebrowser/qutebrowser.py
 abbr --add ubuntu_version lsb_release -idrc
