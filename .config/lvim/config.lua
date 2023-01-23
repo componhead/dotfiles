40713f9b201da2eefa37661bcc339fce07f32e10
Emiliano Anichini <i.hate@spam.bot>
2023-01-23 17:07:21 +0100

    config lvim and vimium

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -183,9 +183,14 @@ linters.setup {
   --   filetypes = { "javascript", "python" },
   -- },
   -- local linters = require "lvim.lsp.null-ls.linters"
-  linters.setup {
     { command = "eslint", filetypes = { "typescript", "typescriptreact" } }
-  }
+}
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "prettier",
+    filetypes = { "javascript", "typescript", "typescriptreact" },
+  },
 }
 
 -- Additional Plugins
