488231713c7397194ee9aa89fa6ffc42e8eadb86
Emiliano Anichini <emiliano.anichini@gmail.com>
2024-01-16 00:01:23 +0100

    fix multiple fish shell configuration loading

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,97 +1,99 @@
 #!/usr/local/bin/fish
 
-# ############# DON'T TOUCH ALL BELOW
-set -xg DOTFILES $HOME/dotfiles
-# see https://standards.freedesktop.org/basiir-spec/basedir-spec-latest.html
-set -xg XDG_CONFIG_HOME $HOME/.config
-set -xg XDG_CONFIG_LINK $DOTFILES/.config
-set -xg XDG_DATA_HOME $HOME/.local/share
-set -xg XDG_DATA_DIRS /usr/local/share/:/usr/share/
-set -xg XDG_CONFIG_DIRS /etc/xdg
-#set -xg XDG_CACHE_HOME $HOME/cache
+if test -z $DOTFILES
+    # ############# DON'T TOUCH ALL BELOW
+    set -xg DOTFILES $HOME/dotfiles
+    # see https://standards.freedesktop.org/basiir-spec/basedir-spec-latest.html
+    set -xg XDG_CONFIG_HOME $HOME/.config
+    set -xg XDG_CONFIG_LINK $DOTFILES/.config
+    set -xg XDG_DATA_HOME $HOME/.local/share
+    set -xg XDG_DATA_DIRS /usr/local/share/:/usr/share/
+    set -xg XDG_CONFIG_DIRS /etc/xdg
+    #set -xg XDG_CACHE_HOME $HOME/cache
 
-set -xg EDITOR nvim
-set -xg GIT_PAGER delta
-set -xg GIT_EDITOR $EDITOR
-set -xg FPP_EDITOR $EDITOR
-set -xg BROWSER '/Applications/Google Chrome.app'
+    set -xg EDITOR nvim
+    set -xg GIT_PAGER delta
+    set -xg GIT_EDITOR $EDITOR
+    set -xg FPP_EDITOR $EDITOR
+    set -xg BROWSER '/Applications/Google Chrome.app'
 
-set -xg NVIM_DIR $XDG_CONFIG_HOME/nvim
-set -xg FISH_DIR $XDG_CONFIG_HOME/fish
-set -xg TMUX_DIR $HOME/.tmux
-set -xg ALACRITTY_DIR $XDG_CONFIG_HOME/alacritty
-set -xg WEZTERM_DIR $XDG_CONFIG_HOME/wezterm
-set -xg ZELLIJ_DIR $XDG_CONFIG_HOME/zellij
-set -xg VIMRC $NVIM_DIR/init.lua
-set -xg GIT_CONFIG_HOME $XDG_CONFIG_HOME/git
-set -xg GIT_CONFIG_GLOBAL $GIT_CONFIG_HOME/config
-set -xg RCFILE $FISH_DIR/config.fish
-set -xg TMUXFILE $HOME/.tmux.conf
-set -xg ZELLIJ_FILE $ZELLIJ_DIR/config.kdl
-# ############# DON'T TOUCH ALL ABOVE
+    set -xg NVIM_DIR $XDG_CONFIG_HOME/nvim
+    set -xg FISH_DIR $XDG_CONFIG_HOME/fish
+    set -xg TMUX_DIR $HOME/.tmux
+    set -xg ALACRITTY_DIR $XDG_CONFIG_HOME/alacritty
+    set -xg WEZTERM_DIR $XDG_CONFIG_HOME/wezterm
+    set -xg ZELLIJ_DIR $XDG_CONFIG_HOME/zellij
+    set -xg VIMRC $NVIM_DIR/init.lua
+    set -xg GIT_CONFIG_HOME $XDG_CONFIG_HOME/git
+    set -xg GIT_CONFIG_GLOBAL $GIT_CONFIG_HOME/config
+    set -xg RCFILE $FISH_DIR/config.fish
+    set -xg TMUXFILE $HOME/.tmux.conf
+    set -xg ZELLIJ_FILE $ZELLIJ_DIR/config.kdl
+    # ############# DON'T TOUCH ALL ABOVE
 
-set -xg APPDATA $HOME
-set -xg CLICOLOR 1
-set -xg CLICOLOR_FORCE 1
-set -xg CPCLIPBOARD pbcopy
-set -xg CPCLIPBOARD_CMD $CPCLIPBOARD -selection clipboard
-set -xg FZF_DEFAULT_OPTS --bind=ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up
-if type -q rg then
-    set -xg FZF_FIND_FILE_COMMAND "rg --files --hidden --follow --glob '!.git'"
+    set -xg APPDATA $HOME
+    set -xg CLICOLOR 1
+    set -xg CLICOLOR_FORCE 1
+    set -xg CPCLIPBOARD pbcopy
+    set -xg CPCLIPBOARD_CMD $CPCLIPBOARD -selection clipboard
+    set -xg FZF_DEFAULT_OPTS --bind=ctrl-d:preview-half-page-down,ctrl-u:preview-half-page-up
+    if type -q rg then
+        set -xg FZF_FIND_FILE_COMMAND "rg --files --hidden --follow --glob '!.git'"
+    end
+    set -xg GIT_CURL_VERBOSE 0
+    set -xg GIT_TRACE 0
+    set -xg GPG_TTY (tty)
+    set -xg GREP_COLOR '3;33'
+    set -xg LANG en_US.UTF-8
+    set -xg LC_CTYPE UTF-8
+    set -xg LESS -R
+    set -xg LESSCHARSET utf-8
+    set -xg LSCOLORS gxcxahdxacafxfaexeabxb
+    set -xg NVIM_PYTHON_LOG_FILE /tmp/log
+    set -xg NVIM_PYTHON_LOG_LEVEL DEBUG
+    set -xg OPENAI_API_HOST api.openai.com
+    set -xg PYTHON /usr/local/bin/python3.9
+    set -xg TMUX_CLIPBOARD_COMMAND "reattach-to-user-namespace pbcopy"
+
+    op signin $OP_ACCOUNT
+    set -xg PRIVATE_DOTFILES $DOTFILES/private_dotfiles/(hostname)
+
+    set -gx PATH $PATH $HOME/bin $DOTFILES/bin
+
+    set -g theme_display_git yes
+    set -g theme_display_git_untracked yes
+    set -g theme_display_git_ahead_verbose yes
+    set -g theme_git_worktree_support yes
+    set -g theme_display_vagrant yes
+    set -g theme_display_docker_machine yes
+    set -g theme_display_hg yes
+    set -g theme_display_virtualenv yes
+    set -g theme_display_ruby yes
+    set -g theme_display_user yes
+    set -g theme_display_vi yes
+    set -g theme_display_date yes
+    set -g theme_display_cmd_duration yes
+    set -g theme_title_display_process yes
+    set -g theme_title_display_path no
+    set -g theme_title_display_user yes
+    set -g theme_title_use_abbreviated_path no
+    set -g theme_date_format "+%Y-%m-%d, %a, %H:%M:%S"
+    set -g theme_avoid_ambiguous_glyphs yes
+    set -g theme_powerline_fonts no
+    set -g theme_nerd_fonts yes
+    set -g theme_show_exit_status yes
+    set -g theme_newline_cursor yes
+    set -g fish_vi_key_bindings yes
+    set -g fish_escape_delay_ms 300
+    set -g fish_user_abbreviations
+    set abbrs_initialized
+    set fish_user_paths /usr/local/bin $HOME/.local/bin
+    set -gx fish_tmux_config $TMUXFILE
+    set fzf_preview_dir_cmd exa --all --color=always
+    set fzf_fd_opts --hidden --exclude=.git
+
+    source $PRIVATE_DOTFILES/privatefile
 end
-set -xg GIT_CURL_VERBOSE 0
-set -xg GIT_TRACE 0
-set -xg GPG_TTY (tty)
-set -xg GREP_COLOR '3;33'
-set -xg LANG en_US.UTF-8
-set -xg LC_CTYPE UTF-8
-set -xg LESS -R
-set -xg LESSCHARSET utf-8
-set -xg LSCOLORS gxcxahdxacafxfaexeabxb
-set -xg NVIM_PYTHON_LOG_FILE /tmp/log
-set -xg NVIM_PYTHON_LOG_LEVEL DEBUG
-set -xg OPENAI_API_HOST api.openai.com
-set -xg PYTHON /usr/local/bin/python3.9
-set -xg TMUX_CLIPBOARD_COMMAND "reattach-to-user-namespace pbcopy"
-
-op signin $OP_ACCOUNT
-set -xg PRIVATE_DOTFILES $DOTFILES/private_dotfiles/(hostname)
-
-set -gx PATH $PATH $HOME/bin $DOTFILES/bin
-
-set -g theme_display_git yes
-set -g theme_display_git_untracked yes
-set -g theme_display_git_ahead_verbose yes
-set -g theme_git_worktree_support yes
-set -g theme_display_vagrant yes
-set -g theme_display_docker_machine yes
-set -g theme_display_hg yes
-set -g theme_display_virtualenv yes
-set -g theme_display_ruby yes
-set -g theme_display_user yes
-set -g theme_display_vi yes
-set -g theme_display_date yes
-set -g theme_display_cmd_duration yes
-set -g theme_title_display_process yes
-set -g theme_title_display_path no
-set -g theme_title_display_user yes
-set -g theme_title_use_abbreviated_path no
-set -g theme_date_format "+%Y-%m-%d, %a, %H:%M:%S"
-set -g theme_avoid_ambiguous_glyphs yes
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
-set -g theme_show_exit_status yes
-set -g theme_newline_cursor yes
-set -g fish_vi_key_bindings yes
-set -g fish_escape_delay_ms 300
-set -g fish_user_abbreviations
-set abbrs_initialized
-set fish_user_paths /usr/local/bin $HOME/.local/bin
-set -gx fish_tmux_config $TMUXFILE
-set fzf_preview_dir_cmd exa --all --color=always
-set fzf_fd_opts --hidden --exclude=.git
-
-source $PRIVATE_DOTFILES/privatefile
 
 # ABBREVIAZIONI DOCKER
 abbr -a dci "docker container info"
@@ -207,8 +209,8 @@ op completion fish | source
 alias gstdiff="git status --porcelain | sed 's/^...//' | while read f; git difftool --color=always --word-diff=color --word-diff-regex=. $argv $f; end;"
 
 if status is-interactive && test -f $FISH_DIR/custom/git_fzf.fish
-	source $FISH_DIR/custom/git_fzf.fish
-	git_fzf_key_bindings
+    source $FISH_DIR/custom/git_fzf.fish
+    git_fzf_key_bindings
 end
 
 set --universal nvm_default_version v18.13.0
