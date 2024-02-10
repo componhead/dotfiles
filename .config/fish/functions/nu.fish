0488a10dab689ac93b149c0082e976402ce59545
Emiliano Anichini <emiliano.anichini@gmail.com>
2024-02-10 18:32:01 +0100

    nushell confi files

--- .config/fish/functions/nu.fish
+++
@@ -1,4 +0,0 @@
-function nu --description 'alias nu nu --config /Users/colossus/dotfiles/.config/nu/config.nu --env-config /Users/colossus/dotfiles/.config/nu/env.nu'
- command nu --config /Users/colossus/dotfiles/.config/nu/config.nu --env-config /Users/colossus/dotfiles/.config/nu/env.nu $argv
-        
-end
