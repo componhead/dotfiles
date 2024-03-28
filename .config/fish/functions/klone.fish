ca19eb64c6f9f82d842e47ac127eb07448a0bf34
Emiliano Anichini <e.anichini@treedom.net>
2024-03-28 14:43:31 +0100

    fix klone script

--- .config/fish/functions/klone.fish
+++ .config/fish/functions/klone.fish
@@ -1,17 +1,17 @@
 function klone -d "Create all structure for a worktree workflow clone"
 # We tell argparse about -h/--help and -s/--second - these are short and long forms of the same option.
     # The "--" here is mandatory, it tells it from where to read the arguments.
-    argparse h/help u/url= r/repodir= b/branch= -- $argv
+    argparse h/help u/url= r/repodir= -- $argv
     # exit if argparse failed because it found an option it didn't recognize - it will print an error
     or return
 
     # If -h or --help is given, we print a little help text and return
     if set -ql _flag_help
-        printf "klone [-h|--help]\nklone -u|--url <url_to_repo>\n      [-r|--repodir <directory_name|last_segment_of_url>]\n      [-b|--branch <branch_to_checkout_from|master>]"
+        printf "klone [-h|--help]\nklone -u|--url <url_to_repo>\n      [-r|--repodir <directory_name|last_segment_of_url>]"
         return 0
     end
 
-    if set -ql _flag_repodir
+    if set -ql _flag_url
         set -f url "$_flag_url"
     else
         echo "url to repo is required"
@@ -21,14 +21,9 @@ function klone -d "Create all structure for a worktree workflow clone"
     if set -ql _flag_repodir
         set -f repo_dir "$_flag_repodir"
     else
-        set -f repo_dir (string split -r "/" "$argv[1]")[-1]
+        set -f repo_dir (string split -r "/" "$url")[-1]
     end
-
-    if set -ql _flag_branch
-        set -f branch "$_flag_branch"
-    else
-        set -f branch $GIT_MAIN_LOCAL
-    end
+    printf "Repo dir: %s\n" "$repo_dir"
 
     set -l bare_dir ".bare"
     mkdir "$repo_dir"
@@ -38,13 +33,14 @@ function klone -d "Create all structure for a worktree workflow clone"
     git clone --bare --recurse-submodules "$url" "$bare_dir"
     echo "gitdir: ./$bare_dir" > .git
 
-    git config "remote.$GIT_MAIN_REMOTE.fetch" "+refs/heads/*:refs/remotes/$GIT_MAIN_REMOTE/*"
+    git config "remote.origin.fetch" "+refs/heads/*:refs/remotes/origin/*"
 
-    git fetch "$GIT_MAIN_REMOTE"
+    git fetch origin
 
-
-    printf "Creating worktree %s\n" "$branch"
-    git worktree add WORKING "$branch"
+    printf "git worktree add ./$directory"
+    set -l main_master (git rev-parse --abbrev-ref HEAD)
+    printf "main_master: %s\n" "$main_master"
+    git worktree add "./WORKING" "$main_master"
 
     if test -e "WORKING/package.json"
         npm i
