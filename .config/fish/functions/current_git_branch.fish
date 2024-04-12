367eb25acf25e5a4bd97badea0b1a46bbd15653d
Emiliano Anichini <e.anichini@treedom.net>
2024-04-12 11:36:03 +0200

    add # to current branch function

--- .config/fish/functions/current_git_branch.fish
+++ .config/fish/functions/current_git_branch.fish
@@ -1,4 +1,4 @@
 function current_git_branch
     set -l branch (git rev-parse --abbrev-ref HEAD)
-    string replace -r ' gb ' (string join '' ' ' (string escape -- $branch) ' ') -- $argv[1]
+    string replace -r ' gb ' (string join '' ' ' \#(string escape -- $branch) ' ') -- $argv[1]
 end
